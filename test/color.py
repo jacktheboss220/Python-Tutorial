6613ab3196a92b58f88d0f5bcf90a08e2aef684c
Mahesh Kumar <jacktheboss2.2.0@gmail.com>
2022-12-25 15:56:33 +0530

    1

--- test/color.py
+++ test/color.py
@@ -1,7 +1,9 @@
-from tkinter import *
+from tkinter import Tk
 from random import randint
 color_changer = Tk()
 
+color_changer.geometry("400x400")
+color_changer.title('color')
 
 def update():
     color = "%05x" % randint(0, 0xFFFFFF)
@@ -10,6 +12,6 @@ def update():
 
 
 update()
-color_changer.title('color')
-color_changer.geometry("400x400")
+
+
 color_changer.mainloop()
